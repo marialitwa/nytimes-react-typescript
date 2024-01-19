8e6075e6664e1d1d0836f91bd9a84779bb90bb9f
Maria Litwa <contact@marialitwa.com>
2024-01-19 10:51:43 +0100

    Test link text decoration styles

--- src/components/ArticleDetails.tsx
+++ src/components/ArticleDetails.tsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import { Article_ArticleSearch } from "../@types/articlesearch";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 
 type Props = {
@@ -28,11 +29,44 @@ export default function ArticleDetails({ article }: Props) {
         <p>Source: {article.source}</p>
         <p>Section: {article.type_of_material}</p>
         <p>Word count: {article.word_count}</p>
-        <Link to={article.web_url} target="_blank" rel="noopener noreferrer">Read full article here</Link>
+        <LinkStyled to={article.web_url} target="_blank" rel="noopener noreferrer">Read full jarticle here</LinkStyled>
         {/* Empfehlung: rel Attribut um Sciherehitsprobleme zu vermeiden => rel="noopener noreferrer" */}
     </div>
   )
 }
+
+
+// STYLING
+
+const LinkStyled = styled(Link)`
+
+  border: 2px solid black;
+  padding: 1em;
+  font-size: 2rem;  
+
+  text-decoration-thickness: 6px;
+  text-decoration-color: hotpink;
+
+  // _text-decoration-style: solid;
+  // _text-decoration-line: line-through;
+  // _text-decoration-line: overline;
+  // _text-decoration: hotpink overline 12px wavy;
+  // _text-decoration-skip-ink: none;
+  // _text-decoration-skip-ink: auto;
+  // _text-decoration-skip-ink: all;
+
+  // _text-underline-offset: 1em;
+
+  // // transition:
+  // //   color 500ms,
+  // //   text-decoration-color 1500ms;
+
+  // //   &:hover {
+  // //     color: lime;
+  // //     text-decoration-color: blue;
+  // //   }
+
+`;
 
 
 
